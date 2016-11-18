fc254d4ec89bf912e51a6f02e4420244f6b74600
zhishui199325 <153691531@qq.com>
2016-11-18 11:02:14 +0800

    刚创建完成

---
+++ app/src/main/java/com/anke/vehicle/kuangjia/MainActivity.java
@@ -0,0 +1,13 @@
+package com.anke.vehicle.kuangjia;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
